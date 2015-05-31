8a7f4d7c40989f35118ccaadebbc569ed949a5a7
Lipi Lee <lipisoft@gmail.com>
2015-06-01 05:34:20 +0900

    Initial commit

---
+++ classfyNetwork.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import urllib2
+import xml.etree.ElementTree as ET
+
+f = open('Address.list', 'r')
+for url in f:
+  try:
+    xml = urllib2.urlopen(url).read()
+    root = ET.fromstring(xml)
+    # FIXME garbage HTTP address is added in node name
+    # check ElementTree.py
+    for netRef in root.iter('{http://www.arin.net/whoisrws/core/v1}netRef'):
+      start = netRef.get('startAddress')
+      end = netRef.get('endAddress')
+      print url, start, end
+  except urllib2.HTTPError, e:
+    print ''
