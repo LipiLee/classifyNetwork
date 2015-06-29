4312f836dd58b82308aad1caff5abd3a46fb4cdc
Lipi C.H. Lee <lipisoft@gmail.com>
2015-06-29 15:16:21 +0900

    Rename script

--- IP-RangeFromIP.py
+++ IP-RangeFromIP.py
@@ -10,7 +10,7 @@ conn.request("POST", "/kor/whois.jsc", params, headers)
 response = conn.getresponse()
 lines = response.read().split('\n')
 for line in lines:
-  if 'CIDR' in line:
+  if 'CIDR' in line or 'IPv4 Address' in line:
     print sys.argv[1], line.split(':')[1].strip()
     break
 conn.close()
